eb56937dd7490b0e95d6a1084c350bbc1c6c1502
brilliant_ember <fire.senfony@gmail.com>
2020-04-10 21:54:58 -0400

    stuff

--- PID_designer.py
+++ PID_designer.py
@@ -1,11 +1,13 @@
 # numpy is weird it has ln as log for some reason
 from numpy import log as ln, arange, roots
-from math import atan, sqrt, pi, degrees
+from math import atan, sqrt, pi, degrees, tan
 from sympy import symbols, simplify, fraction,solve, pprint, evalf, im, I
 from math import isclose
 from control import root_locus, tf, tfdata, feedback
 
-#readability note: sym = symbolic 
+#readability note: sym = symbolic
+# This is based on 2nd order approximation, if you try to use a 3rd order tf, it will give you
+# the 2nd approx, if u input a real 2nd order tf it will give u a true result 
 
 def get_num_den_from_tf(tf):
     x = symbols('x')
@@ -43,13 +45,19 @@ def symbolic_fraction_to_tf(sym_frac):
     num , den = fraction(sym_frac)
     pass
 
+def pick_positive_im_pole(pole_array):
+    for i in pole_array:
+        x = i.imag
+        if x > 0:
+            return x
+    return 0
 def get_symbolic_characteristic_eq(symbolic_tf):
     num, char_eq = fraction(simplify(symbolic_tf))
     return char_eq
 
 '''takes the desired +im(pole) and optf to get the needed P controller gain to meet that requirement,
  if you have a feedback gain then opn tf is feedback gain * opn loop gain'''
-def find_P_controller_gain(positive_cmplx_part_wanted, open_loop_tf, iteration_range = 100, decimal_places=0.01):
+def find_P_controller_gain(positive_cmplx_part_wanted, open_loop_tf, iteration_range = 300, decimal_places=0.01):
     #char eq will always be in the form (Kp+Kds+Ki/s)(opn lp num)+(opn lop den)
     num, den = get_num_den_from_tf(open_loop_tf)
     # we're finding kp first, so kds and ki/s are zero and we loop with different kp
@@ -58,12 +66,16 @@ def find_P_controller_gain(positive_cmplx_part_wanted, open_loop_tf, iteration_r
     Kp_test_list = arange(0,iteration_range,decimal_places)
     last_best = 1000 # random big number to start with
     for k in Kp_test_list:
+        #char eq is always in the form den + H(s)*num where den and num are that of the opn loop tf
         tmp_char_eq = numpy_poly_adder((k*num),den) 
+        
+        current_im_pole = pick_positive_im_pole(roots(tmp_char_eq))
+
         print({
             "k":k,
             "tmp_char_eq":tmp_char_eq,
+            "current pole":current_im_pole
         })
-        current_im_pole = roots(tmp_char_eq)[0].imag
         if( isclose(positive_cmplx_part_wanted, current_im_pole, rel_tol=1e-01, abs_tol=0.17) ):
             current_diff = abs(current_im_pole - positive_cmplx_part_wanted)
             if(current_diff >= last_best ):
@@ -92,84 +104,37 @@ def numpy_poly_adder(poly1,poly2):
             output[l2-1-i]+=poly1[l1-1-i]
     return output  
 
-# def compare_current_to_desired(current, desired):
-#     if(isclose(desired, current, rel_tol=1e-01, abs_tol=0.17)):
-#         current_diff = abs(desired - current)
-#         print("current diff: " +str(current_diff))
-#         print(last_best)
-#         if(current_diff >= last_best ):
-#             print(str(k) + " I am the found kp")
-#             print(str(wanted_pole) + " desired pole im")
-#             print(str(current_cmplx_prt) + "found cmplx part")
-#         else:
-#             last_best = current_diff
 
-    # sym_tf = tf_to_symbolic_fraction(open_loop_tf)
-    # char_eq = get_symbolic_characteristic_eq(sym_tf)
-    # # root_locus_points = root_locus(open_loop_tf, plot=False, )[0]
-    # solve_with_algebra(char_eq,arange(0,20,0.01),positive_cmplx_part_wanted)
+'''takes overshoot (not percentage! ) and gets zeta needed
+to meet that overshoot requirement, ex if overshoot is 20% 
+input 0.2 to the function    and it will output the needed damping ratio to meet the os '''
+def damping_ratio_from_os(os):
+    zeta = -ln(os)/sqrt(pi**2+(ln(os))**2)
+    return zeta
 
-    # root_locus_gains = root_locus(open_loop_tf)[1]
-    # for gain in root_locus_gains:
-    #     eq_to_solve = char_eq + gain
-    #     solved = solve(eq_to_solve)
-    #     # if it's not length 2 then there is no im part
-    #     if len(solved)==2:
-    #         # the positive is the 2nd part(what we want), that's how sympy solves it
-    #         current_cmplx_prt = im(solved[1]).evalf()
 
-    #         if(isclose(positive_cmplx_part_wanted,current_cmplx_prt,rel_tol=1e-02, abs_tol=0.17)):
-    #             print(str(gain) + " I am the found kp")
-    #             print(str(positive_cmplx_part_wanted) + " desired pole im")
-    #             print(str(current_cmplx_prt) + "found cmplx part")
-                
-        # else:
-        #     print(str(gain) + " I am the found kp")
-        #     print(str(positive_cmplx_part_wanted) + " desired pole im")
-        #     print(str(current_cmplx_prt) + "found cmplx part")
-        #     # raise Exception("can't find an accurate enough Kp")
+'''the angle needed to meet the zeta condition, drawn from origin to + pole, and by symmetry can be extended to bottom angle
+taken by drawing a line from the origin to the complex conjugate pole ie the -pole'''
+def angle_from_damping_ratio(zeta):
+    x = sqrt(1-zeta**2)/zeta
+    angle = atan(x)
+    return angle
 
-#scratched idea, was too slow
-# def solve_with_root_locus(open_loop_tf, k_list, wanted_pole):
-#     for pnt in root_locus_points:
-#         current_cmplx_prt = pnt[1].imag
-#         print(current_cmplx_prt)
-#         if current_cmplx_prt != 0:
-#             if(isclose(positive_cmplx_part_wanted,current_cmplx_prt,rel_tol=1e-02, abs_tol=0.17)):
-#                 print(str(gain) + " I am the found kp")
-#                 print(str(positive_cmplx_part_wanted) + " desired pole im")
-#                 print(str(current_cmplx_prt) + "found cmplx part")
+def degree_angle_from_os(os):
+    return(degrees(angle_from_damping_ratio(damping_ratio_from_os(os))))
 
-#scratched idea takes too long
-# def solve_with_algebra(char_eq, k_list, wanted_pole):
-#     pprint(char_eq)
-#     last_best = 1000 # random big number to start with
-#     for k in k_list:
-#         new_char_eq = char_eq + k
-#         solved = solve(new_char_eq)
-#         # if it's not length 2 then there is no im part
-#         if len(solved)==2:
-#             # the positive is the 2nd part(what we want), that's how sympy solves it
-#             current_cmplx_prt = im(solved[1]).evalf()
-#             if(isclose(wanted_pole,current_cmplx_prt,rel_tol=1e-01, abs_tol=0.17)):
-#                 current_diff = abs(wanted_pole - current_cmplx_prt)
-#                 print("current diff: " +str(current_diff))
-#                 print(last_best)
-#                 if(current_diff >= last_best ):
-#                     print(str(k) + " I am the found kp")
-#                     print(str(wanted_pole) + " desired pole im")
-#                     print(str(current_cmplx_prt) + "found cmplx part")
-#                     break
-#                 else:
-#                     last_best = current_diff
-    
-
+'''answer is in radins'''
+def angle_from_os(os):
+    return(angle_from_damping_ratio(damping_ratio_from_os(os)))
 
 ##### Tests####
 s = tf('s')
-Go = 1/(s**2+s)
-# assert that by useing syms funciton syms to veryfy den and num r the same
-symbolic_frac = tf_to_symbolic_fraction(Go)
+Go = 1/( (s+1) *(s+2) *(s+10))
+# # assert that by useing syms funciton syms to veryfy den and num r the same
+# symbolic_frac = tf_to_symbolic_fraction(Go)
 
-find_P_controller_gain(3.89,Go)
+# q = find_P_controller_gain(3.93,Go)
+
+a = tan(angle_from_os(0.2))
+print(a)
 
