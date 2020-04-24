1a710adffee49495bc04f0bd9d3b4bfbbe3de1b7
Mohammed Al <fire.senfony@gmail.com>
2020-04-24 06:15:01 -0400

    Delete PID_designer_test.py
    obsolete

--- PID_designer_test.py
+++
@@ -1,9 +0,0 @@
-s = tf('s')
-Go = 1/(s**2-s)
-# assert that by useing syms funciton syms to veryfy den and num r the same
-symbolic_frac = tf_to_symbolic_fraction(Go)
-pprint(symbolic_frac)
-
-Go = 1/( (s+1) *(s+2) *(s+10))
-q = find_P_controller_gain(3.93,Go)
-# assert that K is 164.9
